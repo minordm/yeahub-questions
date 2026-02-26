4fbd09ae78e51d6657faa9e2f6de2e23953d3f28
minor.dm <lugovoi21@gmail.com>
2026-02-26 17:09:27 +0500

    create proj

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react()],
+});
